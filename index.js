bf4c4c91e9115f7bb9dd5020c53ad9ecf7da7d86
“Gihae <designbygihae@gmail.com>
2021-11-18 20:58:04 -0500

    puppy images

---
+++ index.js
@@ -0,0 +1,36 @@
+const gameBox = document.querySelector('.cardBox')
+
+
+///
+
+
+
+
+
+
+///match card 
+
+
+
+
+
+
+
+
+////flip card 
+
+
+
+
+
+
+
+
+////clear off match cards
+
+
+
+
+
+
+///show a hidden image with notification of winning message
